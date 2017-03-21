db5051bf4fea7c7eb9fb437bcd92fdb92ee1f2ae
UrosMomcilovic <urosuki968@gmail.com>
2017-03-21 14:24:30 +0100

    Mala ispravka

--- BibliotekaHome/src/biblioteka/Autor.java
+++ BibliotekaHome/src/biblioteka/Autor.java
@@ -1,5 +1,41 @@
 package biblioteka;
 
 public class Autor {
+	
+	private String ime,prezime;
 
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof Autor)){
+			throw new RuntimeException();
+		}
+		Autor a = (Autor) obj;
+		if(a.getIme().equals(ime) && a.getPrezime().equals(prezime)){
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
 }
